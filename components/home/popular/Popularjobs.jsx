b86128c6cd89cf0794a2d30cf62b7a55d8579f5c
DorTeri <dortayari@gmail.com>
2023-06-13 17:48:50 +0300

    Added nearby jobs

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -17,6 +17,12 @@ const Popularjobs = () => {
     num_pages: 1
   })
 
+  const [selectedJob, setSelectedJob] = useState()
+
+  const handleCardPress = (item) => {
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -36,7 +42,9 @@ const Popularjobs = () => {
             data={data}
             renderItem={({ item }) => (
               <PopularJobCard 
-              item={item} />
+              item={item} 
+              selectedJob={selectedJob}
+              handleCardPress={handleCardPress}/>
             )}
             keyExtractor={item => item?.job_id}
             contentContainerStyle={{columnGap: SIZES.medium}}
